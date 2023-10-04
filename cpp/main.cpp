7cb1c01d7ab3b70417627be7c2968f917775c7d8
Asmaa Atef <asmaatef6789@gmail.com>
2023-10-04 23:07:40 +0300

    finishing the bw filter

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,51 @@
-//
-cout<<"hello";
-//
+#include<bits/stdc++.h>
+#include "bmplib.cpp"
+
+using namespace std;
+unsigned char image[SIZE][SIZE];
+
+void loadImage () {
+    char imageFileName[100];
+
+    // Get gray scale image file name
+    cout << "Enter the source image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    readGSBMP(imageFileName, image);
+}
+
+void saveImage () {
+    char imageFileName[100];
+
+    // Get gray scale image target file name
+    cout << "Enter the target image file name: ";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeGSBMP(imageFileName, image);
+}
+
+void BlackandWhiteImage() {
+
+    int average , sum=0 , total = 256*256;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< SIZE; j++) {
+            sum += image[i][j];
+        }
+    }
+    average = sum / total;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j< SIZE; j++) {
+            if(image[i][j]>average){
+                image[i][j]= 225;
+            }
+            else{
+                image[i][j]= 0;
+            }
+        }
+    }
+}
+
