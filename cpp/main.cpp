61cd95c93155ab69c4bb682fa36d508a1c69bd27
Aya Ehab Ramadan <124000654+AyA-EhaB@users.noreply.github.com>
2023-10-05 23:30:51 +0300

    Update main.cpp and adding filter 8

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 unsigned char image[SIZE][SIZE];
 unsigned char step[SIZE][SIZE];
 
-void loadImage () {
+void loadImage() {
     char imageFileName[100];
 
     // Get gray scale image file name
@@ -13,11 +13,11 @@ void loadImage () {
     cin >> imageFileName;
 
     // Add to it .bmp extension and load image
-    strcat (imageFileName, ".bmp");
+    strcat(imageFileName, ".bmp");
     readGSBMP(imageFileName, image);
 }
 
-void saveImage () {
+void saveImage() {
     char imageFileName[100];
 
     // Get gray scale image target file name
@@ -25,37 +25,36 @@ void saveImage () {
     cin >> imageFileName;
 
     // Add to it .bmp extension and load image
-    strcat (imageFileName, ".bmp");
+    strcat(imageFileName, ".bmp");
     writeGSBMP(imageFileName, image);
 }
 
 void BlackandWhiteImage() {
 
-    int average , sum=0 , total = 256*256;
+    int average, sum = 0, total = 256 * 256;
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j< SIZE; j++) {
+        for (int j = 0; j < SIZE; j++) {
             sum += image[i][j];
         }
     }
     average = sum / total;
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j< SIZE; j++) {
-            if(image[i][j]>average){
-                image[i][j]= 225;
-            }
-            else{
-                image[i][j]= 0;
+        for (int j = 0; j < SIZE; j++) {
+            if (image[i][j] > average) {
+                image[i][j] = 225;
+            } else {
+                image[i][j] = 0;
             }
         }
     }
 }
 
 void FlipImage() {
-    int flip= SIZE-1;
+    int flip = SIZE - 1;
     string fpdir;
-    cout<<"please enter horizontal or vertical: ";
-    cin>>fpdir;
-    if(fpdir=="horizontal") {
+    cout << "please enter horizontal or vertical: ";
+    cin >> fpdir;
+    if (fpdir == "horizontal") {
         for (int i = SIZE - 1; i >= 0; i--) {
             for (int j = 0; j < SIZE; j++) {
                 step[flip - i][j] = image[i][j];
@@ -67,11 +66,10 @@ void FlipImage() {
                 image[i][j] = step[i][j];
             }
         }
-    }
-    else{
+    } else {
         for (int i = 0; i < SIZE; i++) {
-            for (int j = SIZE-1; j >= 0; j--) {
-                step[i][flip-j] = image[i][j];
+            for (int j = SIZE - 1; j >= 0; j--) {
+                step[i][flip - j] = image[i][j];
             }
         }
 
@@ -82,6 +80,7 @@ void FlipImage() {
         }
     }
 }
+
 //function to rotate the chosen photo
 void Rotate_Image() {
     unsigned char temp[SIZE][SIZE];
@@ -91,16 +90,15 @@ void Rotate_Image() {
         }
     }
     int degree;
-    cout << "Please enter the rotation degree: "<< endl;
+    cout << "Please enter the rotation degree: " << endl;
     cin >> degree;
-    if(degree == 90){
+    if (degree == 90) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 image[i][j] = temp[SIZE - 1 - j][i];
             }
         }
-    }
-    else if(degree == 180){
+    } else if (degree == 180) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 image[i][j] = temp[SIZE - 1 - j][i];
@@ -116,8 +114,7 @@ void Rotate_Image() {
                 image[i][j] = temp[SIZE - 1 - j][i];
             }
         }
-    }
-    else if(degree == 270){
+    } else if (degree == 270) {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 image[i][j] = temp[SIZE - 1 - j][i];
@@ -152,25 +149,63 @@ void Rotate_Image() {
         }
     }
 }
+//-------------------------------------------------------------------------------------------
 //function to invert each pixel of the chosen photo
-void  Invert_Image(){
+void Invert_Image() {
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j< SIZE; j++) {
-            if(image[i][j]==0)image[i][j]= 225;
-            else if (image[i][j]==225)image[i][j]= 0;
-            else image[i][j] =255-image[i][j];
+        for (int j = 0; j < SIZE; j++) {
+            if (image[i][j] == 0)image[i][j] = 225;
+            else if (image[i][j] == 225)image[i][j] = 0;
+            else image[i][j] = 255 - image[i][j];
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------
+//function to divide the photo into 4 quarters and chose one to zoom in to the original size
+void ZOOM_CHOOSEN_QUARTER() {
+    int quarter;
+    unsigned char temp[SIZE / 2][SIZE / 2];
+    cout << "please enter the chosen quarter: ";
+    cin >> quarter;
+    if (quarter == 1) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
+                temp[i][j] = image[i][j];
+            }
+        }
+    } else if (quarter == 2) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
+                temp[i][j] = image[i][j + SIZE / 2];
+            }
+        }
+    } else if (quarter == 3) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
+                temp[i][j] = image[i + SIZE / 2][j];
+            }
+        }
+    } else if (quarter == 4) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            for (int j = 0; j < SIZE / 2; j++) {
+                temp[i][j] = image[i + SIZE / 2][j + SIZE / 2];
             }
         }
     }
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            image[i][j] = temp[i / 2][j / 2];
+        }
+    }
+}
+
 //--------------------------------------------------------------------------------------------
 //the user should be able to choose the filter from a list of filters
-int main()
-{
+int main() {
     loadImage();
     //the list of filters should be here
     saveImage();
 
     return 0;
 }
-
-
